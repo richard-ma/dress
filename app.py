abaff3ddfead1a20776b9e6609e9aa46ef699faf
Richard Ma <richard.ma.19850509@gmail.com>
2017-12-03 21:27:20 +0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+from flask import Flask
 from flask import render_template
-from flask_pluginengine import PluginFlask, PluginEngine
+
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
+
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
 
 # flask plugin engine
+#from flask_pluginengine import PluginFlask, PluginEngine
+'''
 app = PluginFlask(__name__)
 
 app.config['PLUGINENGINE_NAMESPACE'] = 'dress'
@@ -15,23 +24,43 @@ for plugin in active_plugins:
     with plugin.plugin_context():
         app.register_blueprint(plugin.get_blueprint())
 
+'''
 # flask-bootstrap
-from flask_bootstrap import Bootstrap
 Bootstrap(app)
 
 # flask-nav
-from flask_nav import Nav
-from flask_nav.elements import Navbar, View
 topbar = Navbar('',
         View('Home', 'index'),
+        View('Host', 'host'),
+        View('Move', 'move'),
 )
 nav = Nav()
 nav.register_element('top', topbar)
 nav.init_app(app)
 
+# flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///host.db'
+db = SQLAlchemy(app)
+
+class Host(db.Model):
+    __tablename__ = 'host'
+    id
+
 @app.route('/')
 def index():
-    return render_template('index.html', plugins=active_plugins)
+    return render_template('index.html')
+
+# host operation
+@app.route('/host')
+def host():
+    return render_template('host.html')
+
+# move operation
+@app.route('/move')
+def move():
+    return render_template('move.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
