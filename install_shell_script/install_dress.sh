89b524853d6f89e59d86754f29ed16954d5176d1
Richard Ma <richard.ma.19850509@gmail.com>
2017-12-08 08:55:05 +0800

    add install scripts

---
+++ install_shell_script/install_dress.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# create python3 virtualenv
+virtualenv -p python3 dress
+cd dress
+. ./bin/activate
+
+# download dress
+wget -O dress.zip -c https://github.com/richard-ma/dress/archive/master.zip
+unzip dress.zip
+mv dress-master dress
+rm -f dress.zip
+
+# install requirements
+pip install -r dress/requirements.txt
+
+# create database
+python dress/install.py
+
+# run app
+python dress/app.py
