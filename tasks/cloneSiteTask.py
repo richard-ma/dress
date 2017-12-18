7783460d505d0a017ef4223494abefa4bb5c6755
Richard Ma <richard.ma.19850509@gmail.com>
2017-12-18 14:37:41 +0800

    add cloneSiteTask

--- tasks/cloneSiteTask.py
+++ tasks/cloneSiteTask.py
@@ -1,6 +1,8 @@
 from task import Task
 from dress.data.models import Host, Status
 
+import paramiko
+
 class CloneSiteTask(Task):
     source_host = None
     dest_host = None
@@ -10,4 +12,46 @@ class CloneSiteTask(Task):
         self.dest_host = dest_host
 
     def run(self):
-        pass
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        ssh.connect(
+                dest_host.ip,
+                port=dest_host.port,
+                username='root',
+                password=dest_host.pwd
+        )
+
+        self.copyFiles(ssh)
+
+        ssh.close()
+
+    def copyFiles(self, ssh):
+        #stdin, stdout, stderr = ssh.exec_command('command')
+        # pull source host site files
+        ssh.exec_command(
+                'sshpass -p %s scp -o StrictHostKeyChecking=no -p -r root@%s:/home/wwwroot/%s /home/wwwroot/%s' % (
+                    source_host.pwd,
+                    source_host.ip,
+                    source_host.domain,
+                    dest_host.domain
+                    )
+        )
+        # pull source host apache config file
+        ssh.exec_command(
+                'sshpass -p %s scp -o StrictHostKeyChecking=no -p -r root@%s:/usr/local/apache/conf/vhost/%s.conf /usr/local/apache/conf/vhost/%s.conf' % (
+                    source_host.pwd,
+                    source_host.ip,
+                    source_host.domain,
+                    dest_host.domain
+                    )
+        )
+        # pull source host nginx config file
+        ssh.exec_command(
+                'sshpass -p %s scp -o StrictHostKeyChecking=no -p -r root@%s:/usr/local/nginx/conf/vhost/%s.conf /usr/local/nginx/conf/vhost/%s.conf' % (
+                    source_host.pwd,
+                    source_host.ip,
+                    source_host.domain,
+                    dest_host.domain
+                    )
+        )
