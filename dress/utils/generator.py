eaf6c1b4b9573db815ac857525aa77703a759c07
Richard Ma <richard.ma.19850509@gmail.com>
2017-12-21 16:11:43 +0800

    add password generator

---
+++ dress/utils/generator.py
@@ -0,0 +1,13 @@
+class Generator(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def generat():
+        pass
+
+class PasswordGenerator(Generator):
+    @classmethod
+    def generat(password_len):
+        s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+        return "".join(random.sample(s, password_len))
