cb8223cbddc381e244e4a236cbede99c4c5d3782
Richard Ma <richard.ma.19850509@gmail.com>
2017-12-22 15:51:00 +0800

    add password generator tests

--- tests/password_generator_tests.py
+++ tests/password_generator_tests.py
@@ -2,6 +2,7 @@ import unittest
 from flask_testing import TestCase
 import dress
 from dress.utils.generator import PasswordGenerator
+from time import sleep
 
 class PasswordGeneratorTestCase(TestCase):
 
@@ -20,8 +21,12 @@ class PasswordGeneratorTestCase(TestCase):
     def test_password_generator(self):
         password_len = 32
         password = PasswordGenerator.generat(password_len)
-
         self.assertEqual(len(password), password_len)
+
+        password1 = PasswordGenerator.generat(password_len)
+        sleep(1)
+        password2 = PasswordGenerator.generat(password_len)
+        self.assertNotEqual(password1, password2)
 
 if __name__ == '__main__':
     unittest.main()
