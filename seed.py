14146cca3d71c97854edc281db7a92421c643ed6
Richard Ma <richard.ma.19850509@gmail.com>
2017-12-25 22:44:10 +0800

    prefected example host attribute

--- seed.py
+++ seed.py
@@ -3,7 +3,21 @@ from dress.data.models import db, Host, Status
 
 def seed_host_data(database):
     example_host1 = Host()
+    example_host1.ip = '1.1.1.1'
+    example_host1.port = 22
+    example_host1.domain = 'example_host1.domain'
+    example_host1.pwd = 'example_host1 password'
+    example_host1.db_pwd = 'example_host1 database password'
+    example_host1.memo = 'example_host1 memo'
+
     example_host2 = Host()
+    example_host2.ip = '2.2.2.2'
+    example_host2.port = 22
+    example_host2.domain = 'example_host2.domain'
+    example_host2.pwd = 'example_host2 password'
+    example_host2.db_pwd = 'example_host2 database password'
+    example_host2.memo = 'example_host2 memo'
+
     try:
         database.session.add(example_host1)
         database.session.add(example_host2)
