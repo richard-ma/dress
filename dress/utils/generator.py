e0c1821cf7c403dcc7975da1e819285bfbcffd5a
Richard Ma <richard.ma.19850509@gmail.com>
2017-12-28 11:35:06 +0800

    fix #1 使用不需要bash转义的字符生成随机密码

--- dress/utils/generator.py
+++ dress/utils/generator.py
@@ -11,5 +11,6 @@ import random
 class PasswordGenerator(Generator):
     @classmethod
     def generat(cls, password_len):
-        s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+        # fix #1 bash safety characters without escaping
+        s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ,._+:@%/-"
         return "".join(random.sample(s, password_len))
