8a8c6077269279759ea69334005f5610cce657f1
Richard Ma <richard.ma.19850509@gmail.com>
2017-12-31 15:06:21 +0800

    add flask-migrate

--- manager.py
+++ manager.py
@@ -1,10 +1,16 @@
 from dress import app
+from dress.data.models import db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 
+migrate = Migrate(app, db)
 manager = Manager(app)
 
 # use 0.0.0.0 as host IP
 manager.add_command('runserver', Server(host="0.0.0.0"))
+
+# add flask migrate commands
+manager.add_command('db', MigrateCommand)
 
 # test command
 @manager.command
@@ -12,4 +18,5 @@ def hello():
     print('hello')
 
 if __name__ == "__main__":
+    print(app)
     manager.run()
